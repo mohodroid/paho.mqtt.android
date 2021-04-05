f83de4d9fcd1bf02f0a23c44db2badc17ab8a1fc
mirtalebi <Mirtalebi@rayanmeh.co.ir>
2021-04-05 12:07:39 +0430

    Change module name

---
+++ core/src/androidTest/java/com/mohdroid/android/mqtt5/core/TestCaseNotifier.java
@@ -0,0 +1,24 @@
+package com.mohdroid.android.mqtt5.core;
+
+class TestCaseNotifier {
+
+    private Throwable exception;
+
+    public void storeException(Throwable exception) {
+        this.exception = exception;
+    }
+
+    public synchronized void waitForCompletion(long timeout) throws Throwable {
+
+        try {
+            wait(timeout);
+        }
+        catch (InterruptedException ignored) {}
+
+        if (exception != null) {
+            throw exception;
+        }
+
+    }
+
+}
